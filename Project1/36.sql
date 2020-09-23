4ffb3abe24ec06d6dc52ada6e08dc24eb4a0bc0b
Soo Yeon Kim <51169375+estela19@users.noreply.github.com>
2020-09-23 22:29:40 +0900

    Add 36

---
+++ Project1/36.sql
@@ -0,0 +1,11 @@
+#36
+SELECT Trainer.name, Pokemon.name
+FROM Trainer, CatchedPokemon, Pokemon, Evolution
+WHERE Trainer.id = owner_id
+  AND pid = Pokemon.id
+  AND Pokemon.id = after_id
+  AND Pokemon.id NOT IN (SELECT before_id
+                         FROM Evolution)
+  AND Pokemon.id IN (SELECT after_id
+                     FROM Evolution)
+ORDER BY Trainer.name
